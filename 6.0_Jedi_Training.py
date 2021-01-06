fbbcff91857c310d9b454a097deefaf47c07162c
thawngbaw <thawngbaw@urbandaleschools.com>
2021-01-06 09:42:49 -0600

    jedi_training

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -3,7 +3,7 @@ Sign your name:__Bawi Thawng___
 
 
 1.) Convert Binary 101010 to Hexadecimal.
-42
+2A
 2.) Convert Binary 11101001 to Octal.
 351
 3.) Convert Hexadecimal FC to Binary.
@@ -15,7 +15,8 @@ RGB(250,175,189)
 6.) Convert Octal 70 to Binary.
  111000
 7.) Convert RGB (32,128,64) to Hexadecimal.
-208040
+#208040
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
 ASCII
 '''
+
